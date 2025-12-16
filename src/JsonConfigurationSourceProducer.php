045bf38132eaa8bf4e9418899054b008708607c4
Martin Pettersson <martin_pettersson@outlook.com>
2025-12-16 18:00:14 +0100

    feat: initial commit

---
+++ src/JsonConfigurationSourceProducer.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * Copyright (c) 2025 Martin Pettersson
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace N7e;
+
+use N7e\Configuration\ConfigurationSourceInterface;
+use N7e\Configuration\JsonConfigurationSource;
+use Override;
+
+/**
+ * Produces JSON configuration sources.
+ */
+class JsonConfigurationSourceProducer implements ConfigurationSourceProducerInterface
+{
+    #[Override]
+    public function canProduceConfigurationSourceFor(string $url): bool
+    {
+        $parts = parse_url($url);
+
+        return is_array($parts) &&
+               ($parts['scheme'] ?? '') === 'file' &&
+               ! array_key_exists('host', $parts) &&
+               array_key_exists('path', $parts) &&
+               str_ends_with($parts['path'], '.json');
+    }
+
+    #[Override]
+    public function produceConfigurationSourceFor(string $url): ConfigurationSourceInterface
+    {
+        return new JsonConfigurationSource((string) parse_url($url, PHP_URL_PATH));
+    }
+}
---
+++ test/JsonConfigurationSourceProducerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * Copyright (c) 2025 Martin Pettersson
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace N7e;
+
+use PHPUnit\Framework\Attributes\Before;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(JsonConfigurationSourceProducer::class)]
+class JsonConfigurationSourceProducerTest extends TestCase
+{
+    private JsonConfigurationSourceProducer $producer;
+
+    #[Before]
+    public function setUp(): void
+    {
+        $this->producer = new JsonConfigurationSourceProducer();
+    }
+
+    #[Test]
+    public function shouldHandleJsonFileSources(): void
+    {
+        $this->assertTrue($this->producer->canProduceConfigurationSourceFor('file:configuration.json'));
+
+        $this->assertFalse($this->producer->canProduceConfigurationSourceFor('file:configuration.yml'));
+        $this->assertFalse($this->producer->canProduceConfigurationSourceFor('file://host/configuration.json'));
+        $this->assertFalse($this->producer->canProduceConfigurationSourceFor('http://configuration.yml'));
+    }
+
+    #[Test]
+    public function shouldProduceConfigurationSourceWithRelativeFilePath(): void
+    {
+        $configurationSource = $this->producer->produceConfigurationSourceFor('file:test/fixtures/configuration.json');
+
+        $this->assertEquals(['key' => 'value'], $configurationSource->load());
+    }
+}
